032ddb81fcb5a0e56e44a909fb869ef3a5e2b069
Thiago <rp.thiagoluiz@gmail.com>
2020-11-19 16:56:51 -0300

    SimpleLandingPage

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,25 +1,33 @@
 import React from 'react'
-import {
-   Container
-} from './styles'
+
+import { Container } from './styles'
+
+import logoGithub from '../../assets/img/imgLandingPage/github.svg'
+import logoTwitter from '../../assets/img/imgLandingPage/twitter.svg'
+import logoLinkedIn from '../../assets/img/imgLandingPage/linkedin.svg'
+
+
+
 
 
 const LandingPage: React.FC = () => {
    return (
       <Container>
+         
          <h1>Bem-Vindo (a)!</h1>
-         <p>Sou um web developer júnior, (HTML, CSS, JS).Experiência com desenvolvimento utilizando React.js.
-            Possuo conhecimentos em Git, em styled components,SASS e typescript. 
-            Expertise em React Hooks e outros populares workflows da biblioteca. 
-            Esse site foi criado para ser utilizado de portifolio, nele você encontrará diversos projetos.
+         <p>Me chamo Thiago, sou um web developer júnior, (HTML, CSS, JS). <br />
+            Experiência com desenvolvimento utilizando React.js.<br />
+            Possuo conhecimentos em Git, em styled components,SASS e typescript. <br />
+            Expertise em React Hooks e outros populares workflows da biblioteca. <br />
+            Esse site foi criado para ser utilizado de portifolio, nele você encontrará diversos projetos.<br />
             Atualmente o site esta sendo atualizado para ReactJS.
          </p>
 
          
-        <div className="container-icons">
-           <a href="https://github.com/RpThiagoluiz" target="_blank"><img src="../../assets/img/imgLandingPage/github.svg" alt="GitHub RpThiagoLuiz"/></a>
-           <a href="https://twitter.com/RpThiagoluiz" target="_blank" ><img src="../../assets/img/imgLandingPage/twitter.svg" alt="Twitter RpThiagoLuiz"/></a>
-           <a href="https://www.linkedin.com/in/thiago-luiz-0984191a7/" target="_blank" ><img src="../../assets/img/imgLandingPage/linkedin.svg" alt="LinkedIn ThiagoLuiz"/></a>
+        <div className="icons">
+           <a href="https://github.com/RpThiagoluiz" target="blank"><img src={logoGithub} alt="GitHub RpThiagoLuiz"/></a>
+           <a href="https://twitter.com/RpThiagoluiz" target="blank" ><img src={logoTwitter} alt="Twitter RpThiagoLuiz"/></a>
+           <a href="https://www.linkedin.com/in/thiago-luiz-0984191a7/" target="blank" ><img src={logoLinkedIn} alt="LinkedIn ThiagoLuiz"/></a>
         </div>
          
       </Container>
--- src/pages/Landing/styles.ts
+++ src/pages/Landing/styles.ts
@@ -1,7 +1,60 @@
 import styled from 'styled-components'
+import bckImg from '../../assets/img/imgLandingPage/space-1951858.png'
 
 export const Container = styled.div`
 
+   display: flex;
+   text-align: center;
+   justify-content:center;
+   flex-direction: column;
+   padding: 5em;
+   height:100vh;
+   color: white;
+   background-color: rgba(30, 31, 34, 0.75);
+    
+    
+    
 
+
+   :before {
+      content: '';
+      background-image: url(${bckImg});
+      background-repeat: no-repeat;
+      background-position: center;
+      background-size: cover ;
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      z-index: -1;
+      opacity: 0.9;
+   }
+   
+   > h1 {
+      letter-spacing: 12px;
+      font-size:4em;
+      justify-content: center;
+   }
+
+   > p {
+      margin: 1.75em 0;
+      text-align: center;
+      padding:0.75em;
+      line-height: 1.75em;
+      
+   }
+
+   >.icons {
+      > a { 
+         padding: 2.75em;
+         text-decoration:none;
+         > img {
+            width:30px;
+            height:30px;
+         } 
+      }
+   }
 
 `
+
